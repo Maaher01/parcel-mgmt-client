6ad57909e1ca10366e92ec19943abde2672e00ce
Maaher <maaher1145@gmail.com>
2025-12-30 19:00:04 +0600

    Minor change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const baseUrl = axios.create({
-  baseURL: 'https://parcel-mgmt-server-vercel.vercel.app/api',
+  baseURL: 'http://localhost:8000/api',
 })
 
 baseUrl.interceptors.request.use(
